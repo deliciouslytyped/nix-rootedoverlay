84f14bae677d4cb9a311bdedc066e0c91d4f0703
deliciouslytyped <deliciouslytyped>
2019-06-29 00:56:36 +0200

    cleaning

--- extern/nixpkgs-pinned.nix
+++ extern/nixpkgs-pinned.nix
@@ -5,6 +5,3 @@ in
   import (builtins.fetchGit (importJSON ./nixpkgs-pinned.json)) { overlays = [
     (import ./to-upstream.nix)
     ];}
-
-#TODO lazy dirmap in the sense that it only imports files you actually use, and not the whole directory -> probably unsafe stuff
-#dirMap = f: dir: builtins.mapAttrs (name: type: ) (builtins.readDir dir)
