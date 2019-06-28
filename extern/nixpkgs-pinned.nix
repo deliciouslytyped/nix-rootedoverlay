bdd2e6652ff1ae25af236a25b5bc1ca9a4a0b9a7
deliciouslytyped <deliciouslytyped>
2019-06-28 19:08:55 +0200

    small extern refactor

--- extern/nixpkgs-pinned.nix
+++ extern/nixpkgs-pinned.nix
@@ -3,21 +3,7 @@ let
   importJSON = path: builtins.fromJSON (builtins.readFile path);
 in
   import (builtins.fetchGit (importJSON ./nixpkgs-pinned.json)) { overlays = [
-    #TODO upstream this
-    (self: super: {
-      lib = super.lib // rec { 
-        mapDir = filter: f: path:
-          let
-            files = super.lib.filterAttrs filter (builtins.readDir path);
-          in
-            builtins.mapAttrs (n: v: f (path + ("/" + n)) v) files;
-
-        mapDirFiles = f: path:
-          mapDir (n: v: v == "regular") (n: v: f n) path; #TODO pretty sure theres a map filter like tihs somewhere in lib
-
-        mapDirFilesRec = null; #TODO composable mapdirs and mapdirfiles?
-        };
-      })
+    (import ./to-upstream.nix)
     ];}
 
 #TODO lazy dirmap in the sense that it only imports files you actually use, and not the whole directory -> probably unsafe stuff
